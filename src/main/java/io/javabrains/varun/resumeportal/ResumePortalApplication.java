b316e259ad1e5fea2686ad83f7c47f3953487e0b
Varun Jaiswal <varjaisw@cisco.com>
2023-02-24 19:11:18 +0530

    first commit resume portal repo 24 feb

---
+++ src/main/java/io/javabrains/varun/resumeportal/ResumePortalApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.varun.resumeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumePortalApplication.class, args);
+	}
+
+}
